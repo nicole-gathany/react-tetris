bc5d2242b26625e67db41c662c69ff2f62d60e61
ncgmph2014 <nicole.gathany@gmail.com>
2020-02-04 21:59:55 -0500

    styled in Cell.js and wrote some notes about style components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS}>
+    cell
+  </StyledCell>
+);
+
+//I have to say styled components is a really interesting way of styling. it's almost like i'm creating my own library
 
 export default Cell;
--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-export const StyledCell = styled.div``;
+export const StyledCell = styled.div`
+  width: auto;
+  background: rgba(${props => props.color}, 0.8);
+`;
 
 //read about styled components because I don't know anything about that
